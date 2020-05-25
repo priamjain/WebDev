a59451f1db6050b432a28674922242ea408d4c2d
Priam Jain <priamjain8@gmail.com>
2020-05-26 01:26:26 +0530

    Basic structure ready

--- ToDo_List/assets/js/script.js
+++ ToDo_List/assets/js/script.js
@@ -1,6 +1,20 @@
-$("li").click(function(event) {
-	$(this).css({
-		"color" : "grey",
-		"text-decoration":"line-through"
+$("ul").on("click", "li", function(event) {
+	$(this).toggleClass('completed');
+});
+
+$("ul").on("click", "li span", function(event) {
+	/* Act on the event */
+	$(this).parent().fadeOut('400', function() {
+		$(this).remove();	
 	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	/* Act on the event */
+	if(event.which === 13){
+		var newtodo = $(this).val();
+		$("ul").append("<li><span>X</span>"+newtodo+"</li>");
+		$(this).val("");
+	}
 });
